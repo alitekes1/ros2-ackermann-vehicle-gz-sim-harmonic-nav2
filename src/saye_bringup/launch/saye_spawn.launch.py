21198c045e791e9e13be10b05bb820f420b18b7f
alitekes1 <ali.tekes@ogr.sakarya.edu.tr>
2024-09-24 00:48:03 +0300

    update yaml file for navigation

--- src/saye_bringup/launch/saye_spawn.launch.py
+++ src/saye_bringup/launch/saye_spawn.launch.py
@@ -69,6 +69,6 @@ def generate_launch_description():
         DeclareLaunchArgument('rviz', default_value='true',
                               description='Open RViz.'),
         bridge,
-        robot_state_publisher,
-        rviz
+        robot_state_publisher
+        # rviz
     ])
